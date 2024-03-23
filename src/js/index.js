e8cd434e33aebb08f8f76d4afac392411bc6cb1f
karel <kareltrisnanto26@gmail.com>
2024-03-23 17:02:31 +0700

    add: hero section carousel

--- src/js/index.js
+++ src/js/index.js
@@ -1,52 +1,66 @@
 $(document).ready(function () {
-  // Dark Mode Feature
-  let theme = localStorage.getItem("theme") || "dark";
-
-  // Function to toggle dark mode
-  function toggleDarkMode() {
-    theme = theme === "dark" ? "light" : "dark";
-    if (theme === "dark") {
-      $("html").attr("data-bs-theme", "dark");
-      $("#toggleIcon").attr("class", "fa-solid fa-moon");
-      localStorage.setItem("theme", "dark");
+  // Navbar Behavior
+  $(window).scroll(function () {
+    var $navbar = $("#navbar");
+    $navbar.toggleClass("scrolled", $(this).scrollTop() > $navbar.height());
+    if ($(this).scrollTop() > $navbar.height()) {
+      $(".nav-link").css("color", "#000");
+      $(".navbar-toggler").css("color", "#000");
     } else {
-      $("html").attr("data-bs-theme", "light");
-      $("#toggleIcon").attr("class", "fa-solid fa-sun");
-      localStorage.setItem("theme", "light");
+      $(".nav-link").css("color", "#fff");
+      $(".navbar-toggler").css("color", "#fff");
     }
+  });
+
+  // Carousel Behavior
+  let nextDom = $("#next");
+  let prevDom = $("#prev");
+  let carouselDom = $(".carousel");
+  let SliderDom = carouselDom.find(".list");
+  let thumbnailBorderDom = $(".carousel .thumbnail");
+  let thumbnailItemsDom = thumbnailBorderDom.find(".item");
+  let timeDom = $(".carousel .time");
+
+  thumbnailBorderDom.append(thumbnailItemsDom.first());
+  let timeRunning = 3000;
+  let timeAutoNext = 7000;
+
+  // Event handlers for next and prev buttons
+  nextDom.click(function () {
+    showSlider("next");
+  });
+
+  prevDom.click(function () {
+    showSlider("prev");
+  });
+
+  let runTimeOut;
+  let runNextAuto = setTimeout(() => {
+    nextDom.click();
+  }, timeAutoNext);
+
+  function showSlider(type) {
+    let SliderItemsDom = SliderDom.find(".item");
+    let thumbnailItemsDom = $(".carousel .thumbnail .item");
+
+    if (type === "next") {
+      SliderDom.append(SliderItemsDom.first());
+      thumbnailBorderDom.append(thumbnailItemsDom.first());
+      carouselDom.addClass("next");
+    } else {
+      SliderDom.prepend(SliderItemsDom.last());
+      thumbnailBorderDom.prepend(thumbnailItemsDom.last());
+      carouselDom.addClass("prev");
+    }
+
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+      carouselDom.removeClass("next prev");
+    }, timeRunning);
+
+    clearTimeout(runNextAuto);
+    runNextAuto = setTimeout(() => {
+      nextDom.click();
+    }, timeAutoNext);
   }
-
-  // Click event listener for toggling dark mode
-  $("#toggleDark").click(toggleDarkMode);
-
-  // Apply theme on page load
-  if (
-    localStorage.theme === "dark" ||
-    (!("theme" in localStorage) &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches)
-  ) {
-    $("html").attr("data-bs-theme", "dark");
-    $("#toggleIcon").attr("class", "fa-solid fa-moon");
-  } else {
-    $("html").attr("data-bs-theme", "light");
-    $("#toggleIcon").attr("class", "fa-solid fa-sun");
-  }
-
-  // Update localStorage when user explicitly chooses light mode
-  $("#lightMode").click(function () {
-    localStorage.setItem("theme", "light");
-    toggleDarkMode();
-  });
-
-  // Update localStorage when user explicitly chooses dark mode
-  $("#darkMode").click(function () {
-    localStorage.setItem("theme", "dark");
-    toggleDarkMode();
-  });
-
-  // Remove theme preference from localStorage when user chooses to respect OS preference
-  $("#autoMode").click(function () {
-    localStorage.removeItem("theme");
-    toggleDarkMode();
-  });
 });
