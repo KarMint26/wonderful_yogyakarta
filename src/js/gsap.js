eb244ba9a36b3159d3b22e108a07281a16bfc75a
karel <kareltrisnanto26@gmail.com>
2024-03-23 23:39:53 +0700

    add: gsap effect

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -1,4 +1,6 @@
-gsap.from(".top-text-head", 1.2, {opacity: 0, x:150, delay: 0.5})
-gsap.from(".bot-text-head", 1.2, {opacity: 0, x:-150, delay: 0.8})
-gsap.from(".desc-about", 1.2, {opacity: 0, x:150, delay: 1.1})
-gsap.from(".img-maps", 1.4, {opacity: 0, y:150, delay: 1})
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".top-text-head", 1.2, { opacity: 0, x: 150, delay: 0.5, scrollTrigger: top });
+gsap.from(".bot-text-head", 1.2, { opacity: 0, x: -150, delay: 0.8, scrollTrigger: top });
+gsap.from(".desc-about", 1.2, { opacity: 0, x: 150, delay: 1.1, scrollTrigger: top });
+gsap.from(".img-maps", 1.4, { opacity: 0, y: 150, delay: 1, scrollTrigger: top });
