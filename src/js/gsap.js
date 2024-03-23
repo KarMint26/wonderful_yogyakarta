db43987c5dbed4421ac1e3f6233e9f79e8b58bfb
Karel Trisnanto Utomo <115652706+KarMint26@users.noreply.github.com>
2024-03-24 00:42:49 +0700

    Update gsap.js

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -1,6 +1,6 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.from(".top-text-head", 1.2, { opacity: 0, x: 150, delay: 0.5, scrollTrigger: top });
-gsap.from(".bot-text-head", 1.2, { opacity: 0, x: -150, delay: 0.8, scrollTrigger: top });
-gsap.from(".desc-about", 1.2, { opacity: 0, x: 150, delay: 1.1, scrollTrigger: top });
-gsap.from(".img-maps", 1.4, { opacity: 0, y: 150, delay: 1, scrollTrigger: top });
+gsap.from(".top-text-head", 1.2, { opacity: 0, x: 50, delay: 0.5, scrollTrigger: top });
+gsap.from(".bot-text-head", 1.2, { opacity: 0, x: -50, delay: 0.8, scrollTrigger: top });
+gsap.from(".desc-about", 1.2, { opacity: 0, x: 50, delay: 1.1, scrollTrigger: top });
+gsap.from(".img-maps", 1.4, { opacity: 0, y: 100, delay: 1, scrollTrigger: top });
