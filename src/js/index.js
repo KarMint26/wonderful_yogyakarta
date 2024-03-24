d83ec62ca5880756345bcca97c38373be1912cf0
karel <kareltrisnanto26@gmail.com>
2024-03-24 23:45:38 +0700

    add: carousel hotels

--- src/js/index.js
+++ src/js/index.js
@@ -52,7 +52,7 @@ $(document).ready(function () {
   });
 
   let runTimeOut;
-  let runNextAuto = setTimeout(() => {
+  let runNextAuto = setTimeout(function () {
     nextDom.click();
   }, timeAutoNext);
 
@@ -71,12 +71,12 @@ $(document).ready(function () {
     }
 
     clearTimeout(runTimeOut);
-    runTimeOut = setTimeout(() => {
+    runTimeOut = setTimeout(function () {
       carouselDom.removeClass("next prev");
     }, timeRunning);
 
     clearTimeout(runNextAuto);
-    runNextAuto = setTimeout(() => {
+    runNextAuto = setTimeout(function () {
       nextDom.click();
     }, timeAutoNext);
   }
@@ -102,12 +102,69 @@ $(document).ready(function () {
 
   $(window).on("scroll resize load", handleScroll);
 
-  $(".button-backtop").click(() => {
+  $(".button-backtop").click(function () {
     $("html, body").animate(
       {
         scrollTop: 0,
       },
       "smooth"
+    );
+  });
+
+  // carousel
+  var carouselContainer = $(".carousel-container");
+  var prevArrow = $(".prev-arrow");
+  var nextArrow = $(".next-arrow");
+
+  prevArrow.on("click", function () {
+    var currentScroll = carouselContainer.scrollLeft();
+    carouselContainer.animate(
+      {
+        scrollLeft: currentScroll - carouselContainer.width(),
+      },
+      500
+    ); // 500 adalah durasi animasi dalam milidetik
+  });
+
+  nextArrow.on("click", function () {
+    var currentScroll = carouselContainer.scrollLeft();
+    carouselContainer.animate(
+      {
+        scrollLeft: currentScroll + carouselContainer.width(),
+      },
+      500
+    ); // 500 adalah durasi animasi dalam milidetik
+  });
+
+  // linked carousel card
+  $("#carousel-1").click(() => {
+    window.open(
+      "https://www.tiket.com/hotel/indonesia/innside-by-melia-yogyakarta-108001534490367057?adult=1&room=1"
+    );
+  });
+  $("#carousel-2").click(() => {
+    window.open(
+      "https://www.tiket.com/hotel/indonesia/merapi-merbabu-hotel-yogyakarta-510001665376232226?adult=1&room=1"
+    );
+  });
+  $("#carousel-3").click(() => {
+    window.open(
+      "https://www.tiket.com/hotel/indonesia/melia-purosani-108001534490283896?adult=1&room=1"
+    );
+  });
+  $("#carousel-4").click(() => {
+    window.open(
+      "https://www.tiket.com/hotel/indonesia/el-hotel-yogyakarta-malioboro-603001680073242229?adult=1&room=1"
+    );
+  });
+  $("#carousel-5").click(() => {
+    window.open(
+      "https://www.tiket.com/hotel/indonesia/royal-ambarrukmo-yogyakarta-108001534490281925?adult=1&room=1"
+    );
+  });
+  $("#carousel-6").click(() => {
+    window.open(
+      "https://www.tiket.com/hotel/indonesia/next-hotel-yogyakarta-505001652429738663?adult=1&room=1"
     );
   });
 });
