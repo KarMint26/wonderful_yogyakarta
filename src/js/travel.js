9708bb7810d8d5cddea051506d217d5e184884ac
dodichakill <dodiherlangga91@gmail.com>
2024-03-27 20:18:11 +0700

    add UI page travel

---
+++ src/js/travel.js
@@ -0,0 +1,222 @@
+const dataTravelsSleman = [
+  {
+    id: 1,
+    title: "Merapi Lava Tour",
+    image: "https://techtitans.sirv.com/wonderful-yogyakarta/travel/s-mlt.png",
+    price: "IDR 350.000 - 550.000",
+    desc: "Jeep Tour or also known as Lava Tour is a tour for visitors who want to experience the sensation of riding a jeep enjoying the panorama of Mount Merapi along the eruption site. This tour is guided by an operator who will explain the area former eruption of Merapi.",
+    time: "07.00-16.00 WIB",
+    location: "ngipiksari, hargobinangun, pakem, sleman, yogyakarta",
+    maps: "https://maps.app.goo.gl/V4tGD5Bx1DZDYNob7",
+  },
+  {
+    id: 2,
+    title: "Ullen Sentalu Museum",
+    image: "https://techtitans.sirv.com/wonderful-yogyakarta/travel/s-usm.png",
+    price: "IDR 40.000",
+    desc: "The Ullen Sentalu Museum is a cultural museum of the Mataram Kingdom. It has many superior collections about the life of the Mataram Dynasty nobles such as their Javanese art and culture",
+    time: "08.30-17.00",
+    location: "Jl. Boyong, Hargobinangun, Pakem, Sleman, Yogyakarta",
+    maps: "https://maps.app.goo.gl/rABbaiG8FKCKYMHg8",
+  },
+  {
+    id: 3,
+    title: "Kaliurang",
+    image: "https://techtitans.sirv.com/wonderful-yogyakarta/travel/s-k.png",
+    price: "IDR 10.000 - 25.000",
+    desc: "Kaliurang is a resort located on the slopes of Mount Merapi. Visitors can visit this complex by trekking to the National Park and Telaga Putri. Apart from that, visitors can rent a jeep to get to the location. Kaliadem bunker.",
+    time: "8.00-17.00 WIB",
+    location: "Kaliurang, Hargobinangun, Pakem, Sleman, Yogyakarta",
+    maps: "https://maps.app.goo.gl/qZrfxQJnsji9FRy49",
+  },
+  {
+    id: 4,
+    title: "Merapi Volcano Museum",
+    image: "https://techtitans.sirv.com/wonderful-yogyakarta/travel/s-mvm.png",
+    price: "IDR 10.000",
+    desc: "This museum was founded as a record of the eruption of Mount Merapi and was inaugurated in 2010.",
+    time: "8.00-15.00 WIB",
+    location: "Bull, Hargobinangun, Pakem, Sleman, Yogyakarta",
+    maps: "https://maps.app.goo.gl/YTw7oUuRiNrhQHW56",
+  },
+];
+
+const dataTravelsBantul = [
+  {
+    id: 1,
+    title: "Parangritis Beach",
+    image: "https://techtitans.sirv.com/wonderful-yogyakarta/travel/b-pb.png",
+    price: "IDR 4.000",
+    desc: "This beach is located approximately ± 27 km from the city center. Become one of the icons of Yogyakarta. This place is popular among tourists for its golden sunsets",
+    time: "24 hours",
+    location: "JI. Parangtritis, Kretek, Bantul, Yogyakarta",
+    maps: "https://maps.app.goo.gl/TzJfePNBvtUfHzxPA",
+  },
+  {
+    id: 2,
+    title: "Imogiri Cimetery",
+    image: "https://techtitans.sirv.com/wonderful-yogyakarta/travel/b-ic.png",
+    price: "IDR 50.000",
+    desc: "This is the tomb of the Sultan of Mataram and is considered a holy place. Visitors can visit this tomb wearing traditional Javanese clothing traditional clothes. It is only open on Monday, Friday, Sunday.",
+    time: "08.00-17.00 WIB",
+    location: "Karang Kulon, Wukirsari, Imogiri, Bantul, Yogyakarta",
+    maps: "https://maps.app.goo.gl/k8i3VmMQhiAnoiDYA",
+  },
+  {
+    id: 3,
+    title: "Mataram Cultural Tourism Area",
+    image: "https://techtitans.sirv.com/wonderful-yogyakarta/travel/b-mcta.png",
+    price: "IDR 5.000",
+    desc: "This area is believed to be a historical site of the Mataram Kingdom and is now a tourist attraction because of its natural scenery. Many destinations around the area are popular such as Mangunan Fruit Plantation, Pine Forest, Sewu Watu Songgo Langit, and Bukit Lintang Sewu",
+    time: "05.00-17.00 WIB",
+    location: "Pantirejo, Terong, Dlingo, Bantul, Yogyakarta",
+    maps: "https://maps.app.goo.gl/ig2oxofJTMR9Zcfw9",
+  },
+  {
+    id: 4,
+    title: "Kasongan",
+    image: "https://techtitans.sirv.com/wonderful-yogyakarta/travel/b-k.png",
+    price: "free",
+    desc: "Kasongan is a collection of pottery and ceramic crafts located in Bangunjiwo village, Bantul Regency. There Many craftsmen around this area make them Kasongan has become an educational tourism area.",
+    time: "Tuesday-Sunday 11.00-16.00 WIB",
+    location: "JI. Kasongan, Bangunjiwo, Bantul Yogyakarta",
+    maps: "https://maps.app.goo.gl/wcKHpisdJFqyGNeP7",
+  },
+];
+
+const dataTravelsKulonprogo = [
+  {
+    id: 1,
+    title: "Glagah Beach",
+    image: "https://techtitans.sirv.com/wonderful-yogyakarta/travel/k-gb.png",
+    price: "IDR 4.000",
+    desc: "Glagah Beach is the main tourist attractions in Kulon Progo that has lagoon. Besides, there is a pier on the beach equipped with tetrappods and sea walls so visitors can enjoy the sensation of breaking the waves here.",
+    time: "24 hours",
+    location: "Glagah, Temon,Kulon Progo, Yogyakarta",
+    maps: "https://maps.app.goo.gl/wZiuB5xYEgvGSNFs8",
+  },
+  {
+    id: 2,
+    title: "Progo Rafting",
+    image: "https://techtitans.sirv.com/wonderful-yogyakarta/travel/k-pr.png",
+    price: "IDR 100.000 - 175.000",
+    desc: "The adventure way to enjoy Progo river is by doing rafting. Progo river Rafting is a sports tourism for tourists who want a thrilling adventure experience and testing adrenaline along the swift currents of the river Progo.",
+    time: "06.00-18.00 WIB",
+    location: "Krisik, kreo, Kalibawang,Kulon Progo,Yogyakarta",
+    maps: "https://maps.app.goo.gl/cud2444ycwBX1d4h8",
+  },
+  {
+    id: 3,
+    title: "Kalibiru",
+    image: "https://techtitans.sirv.com/wonderful-yogyakarta/travel/k-wk.png",
+    price: "IDR 20.000",
+    desc: "Deskripsi : Kalibiru is located in the Menoreh hill. This Tourism Village is an ecotourism object that has beautiful scenery and natural landscape of Sermo Reservoir seen from a height. ",
+    time: "08.00-17.00 WIB",
+    location: "Waduk Sermo, Kokap, Kulon Progo, Yogyakarta",
+    maps: "https://maps.app.goo.gl/qt66imKpLFQrQAn56",
+  },
+  {
+    id: 4,
+    title: "Nglinggo Tea",
+    image: "https://techtitans.sirv.com/wonderful-yogyakarta/travel/k-nt.png",
+    price: "IDR 5.000",
+    desc: "Nglinggo Tea Plantation is a tourism object has a stretch of 136 hectares of tea plantations at an altitude of ± 900-1000 meters above sea level. Visitors can enjoy the beauty panorama of this tea plantation by using a jeep or motorbike trail.",
+    time: "07.00-18.00 WIB",
+    location: "JI. Kasongan, Bangunjiwo, Bantul Yogyakarta",
+    maps: "https://maps.app.goo.gl/vHoEvFk9gJzU8Kmg9",
+  },
+];
+
+const dataTravelsGunungkidul = [
+  {
+    id: 1,
+    title: "Nglanggeran",
+    image: "https://techtitans.sirv.com/wonderful-yogyakarta/travel/w-n.png",
+    price: "IDR 30.000",
+    desc: "Nglanggeran is an ancient volcano area that can be used for trekking. Visitors who want to see the natural scenery of mountain ranges in the Gunung Sewu Geopark cluster are enable to trek to the top of the mountain.",
+    time: "08.00-17.00 WIB",
+    location: "Nglanggeran, Patuk, Gunung Kidul, Yogyakarta",
+    maps: "https://maps.app.goo.gl/MLPmg28J4nCV8zny8",
+  },
+  {
+    id: 2,
+    title: "Timang Beach",
+    image: "https://techtitans.sirv.com/wonderful-yogyakarta/travel/w-tb.png",
+    price: "IDR 100.000",
+    desc: "Timang Beach is a beautiful beach in Gunungkidul which has large stones in the middle of the sea which is often called Pulau Panjang.",
+    time: "06.00-18.00 WIB",
+    location: "Tepus, Gunungkidul, Yogyakarta",
+    maps: "https://maps.app.goo.gl/RcEjR2Y5kaHBQe1V6",
+  },
+  {
+    id: 3,
+    title: "Jomblang Cave",
+    image: "https://techtitans.sirv.com/wonderful-yogyakarta/travel/w-gj.png",
+    price: "IDR 50.000",
+    desc: 'Enjoy the "light around Goa Jomblang of ±heaven" 50 meters in Jomblang and go enter cave, avisitors depth around must enter ± 90 themeters cave from usingthe themouth SingleofRope the cave Technique with a (SRT) provided by the operator.',
+    time: "09.15-12.30 WIB",
+    location: "Jetis Wetan, Semanu, Pancarejo Gunung Kidul, Yogyakarta",
+    maps: "https://maps.app.goo.gl/GxPVrPos3P98jeGh9",
+  },
+  {
+    id: 4,
+    title: "Sri Gethuk Waterfall",
+    image: "https://techtitans.sirv.com/wonderful-yogyakarta/travel/w-sg.png",
+    price: "IDR 25.000",
+    desc: "Sri Gethuk Waterfall is located between 2 cliffs with a 50 meters height. Visitors can swim near the rocks of the waterfall or down the river using a canoe in the waterfall area.",
+    time: "08.00-16.00 WIB",
+    location: "Mungguran II, Bleberan, Playen, Gunungkidul, Yogyakarta",
+    maps: "https://maps.app.goo.gl/JE7j91iUyBuZkgb88",
+  },
+];
+
+const tabSleman = document.querySelector(".travels-sleman");
+const tabBantul = document.querySelector(".travels-bantul");
+const tabKulonprogo = document.querySelector(".travels-kulonprogo");
+const tabGunungkidul = document.querySelector(".travels-gunungkidul");
+
+const renderCard = (listData, container) =>
+  listData.forEach((data) => {
+    let card;
+    if (!(data.id % 2 === 0)) {
+      card = `
+        <div class="card card-travel rounded-4 p-3">
+            <div class="d-flex gap-3">
+              <div class="img-container w-50">
+                <img src=${data.image} alt="sleman" class="object-fit-cover rounded-5" style="width: 100%; height: 100%;" />
+              </div>
+              <div class="d-flex flex-column w-50">
+                <h1 class="mt-2">${data.title}</h1>
+                <p style="padding-right: .5rem;">${data.desc}</p>
+                <div class="desc-info"><i class="bi bi-clock-fill"></i><span>${data.time}</span></div>
+                <div class="desc-info"><i class="bi bi-cash"></i><span>± ${data.price}</span></div>
+                <div class="desc-info" style="margin-bottom: 1rem;"><i class="bi bi-geo-alt-fill"></i><span>${data.location}</span></div>
+                <a href=${data.maps} target="_blank" class="btn-primary" style="width: 10.5rem;"> <i class="bi bi-eye-fill h6" style="margin-right: .5rem;"></i> Visit Now</a>
+              </div>
+            </div>
+          </div>
+        `;
+    } else {
+      card = `<div class="card card-travel card-travel-second rounded-4 p-3">
+        <div class="d-flex gap-3">
+          <div class="d-flex flex-column w-50">
+            <h1 class="mt-2 card-left-p">${data.title}</h1>
+            <p style="padding-right: .5rem;" class="card-left-p">${data.desc}</p>
+            <div class="desc-info card-left-p"><i class="bi bi-clock-fill"></i><span>${data.time}</span></div>
+            <div class="desc-info card-left-p"><i class="bi bi-cash"></i><span>± ${data.price}</span></div>
+            <div class="desc-info card-left-p" style="margin-bottom: 1rem;"><i class="bi bi-geo-alt-fill"></i><span> ${data.location} </span></div>
+            <a href=${data.maps} target="_blank" class="btn-primary card-left-p" style="width: 10.5rem;"> <i class="bi bi-eye-fill h6" style="margin-right: .5rem;"></i> Visit Now</a>
+          </div>
+          <div class="img-container w-50">
+            <img src=${data.image} alt="sleman" class="object-fit-cover" style="width: 100%; height: 100%;" />
+          </div>
+        </div>
+      </div>`;
+    }
+    container.innerHTML += card;
+  });
+
+renderCard(dataTravelsSleman, tabSleman);
+renderCard(dataTravelsSleman, tabGunungkidul);
+renderCard(dataTravelsKulonprogo, tabKulonprogo);
+renderCard(dataTravelsBantul, tabBantul);
