7c60fd3fed0b2928cc8302dde38f36fc4de5efd1
karel <kareltrisnanto26@gmail.com>
2024-04-06 15:50:33 +0700

    add: culinary section

--- src/js/culinary.js
+++ src/js/culinary.js
@@ -1,3 +1,34 @@
 injectNavbar("culinary");
 
+// Gudeg
+$("#head-a-1").click(() => {
+  $("#head-p-1").toggleClass("active");
+  $("#head-p-2").toggleClass("active");
+});
 
+$("#head-a-2").click(() => {
+  $("#head-p-1").toggleClass("active");
+  $("#head-p-2").toggleClass("active");
+});
+
+// Bakmi Jawa
+$("#head-a-3").click(() => {
+  $("#head-p-3").toggleClass("active");
+  $("#head-p-4").toggleClass("active");
+});
+
+$("#head-a-4").click(() => {
+  $("#head-p-3").toggleClass("active");
+  $("#head-p-4").toggleClass("active");
+});
+
+// Sate Klatak
+$("#head-a-5").click(() => {
+  $("#head-p-5").toggleClass("active");
+  $("#head-p-6").toggleClass("active");
+});
+
+$("#head-a-6").click(() => {
+  $("#head-p-5").toggleClass("active");
+  $("#head-p-6").toggleClass("active");
+});
