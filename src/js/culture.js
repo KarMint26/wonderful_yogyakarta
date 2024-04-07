7adb02522710260608906e0783a0ba2f8eccf7b5
dodichakill <dodiherlangga91@gmail.com>
2024-04-07 12:04:19 +0700

    refactore: UI Btn

--- src/js/culture.js
+++ src/js/culture.js
@@ -185,7 +185,7 @@ const renderCard = (listData, container) =>
                     data.yt
                   }" title='${
         data.title
-      }' frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      }' frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen autoplay></iframe>
                   </div>
                 </div>
               </div>
